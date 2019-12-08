2ef05dcaec546c3c1cb789eed721f064e2ba6fc4
loading <49134864+load1n9@users.noreply.github.com>
2019-12-08 00:41:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import json
+from markdown2 import markdown
 
+config = open("config.json", "r")	
+
+configurated = json.load(config)	
+
+sitefile = open("templates/stuff.html", "r")	
+
+data = sitefile.read()	
+data = data.replace("///site", configurated["site"])	
+sitefile = open("pages/index.html", "w")	
+sitefile.write(data)	
+sitefile.close()
